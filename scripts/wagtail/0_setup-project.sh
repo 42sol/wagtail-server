d00f88120d0d17378d643eb18e44a3d8abe5a4c3
Felix Häberle <felix@42sol.eu>
2017-08-12 20:50:17 +0200

    script to create a new wagtail project (use parameter1 to set the name)

---
+++ scripts/wagtail/0_setup-project.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [-z "$1"] 
+then
+  jumpto error1:
+fi
+if [-d "$1"] 
+then
+  jumpto error2:
+fi
+
+
+echo "-[1] setup project (parameter-1=$1)"
+wagtail start $1 # setup the project
+cd $1            # setup the database
+echo "-[2] prepare for running (migrate)"
+python3 manage.py migrate
+echo "-[3] create a superuser account"
+echo "     hit CTRL-C if all is fine"
+echo ""
+python3 manage.py createsuperuser # create an administrator account
+echo "-[4] run the development server - only to see if all is fine"
+python3 manage.py runserver
+echo "-[5] copy script to project folder"
+cp ../*.sh .
+rm -f 0_*.sh # this script is not needed here
+echo "-[6] back to apps folder"
+cd ..
+jumpto end
+
+error1:
+echo "-[error1] invalid parameter-1=$1 - is empty
+
+error2:
+echo "-[error2] invalid parameter-1=$1 - already exists
+
+end:
