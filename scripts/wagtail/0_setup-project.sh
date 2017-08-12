23ad1cc4ab702667a52a89612a497fe6de6e1fdb
Felix Häberle <felix@42sol.eu>
2017-08-12 21:34:23 +0200

    I am still unable to write bash scripts --> maybe use python

--- scripts/wagtail/0_setup-project.sh
+++ scripts/wagtail/0_setup-project.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-if [-z "$1"] 
-then
-  jumpto error1:
-fi
-if [-d "$1"] 
-then
-  jumpto error2:
-fi
-
+#if [-z "$1"] 
+#then
+#  echo "-[error1] invalid parameter-1=$1 - is empty"
+#  return 1
+#fi
+#
+#if [-d "$1"] 
+#then
+#  echo "-[error2] invalid parameter-1=$1 - already exists"
+#  return 2
+#fi
 
 echo "-[1] setup project (parameter-1=$1)"
 wagtail start $1 # setup the project
@@ -26,12 +28,3 @@ cp ../*.sh .
 rm -f 0_*.sh # this script is not needed here
 echo "-[6] back to apps folder"
 cd ..
-jumpto end
-
-error1:
-echo "-[error1] invalid parameter-1=$1 - is empty
-
-error2:
-echo "-[error2] invalid parameter-1=$1 - already exists
-
-end:
